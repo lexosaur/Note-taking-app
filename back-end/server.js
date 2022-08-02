921f23e68572c7586637229ec9033a372a76dc9b
Alexis García <alexis.garciad@gmail.com>
2022-08-01 21:27:12 -0500

    server connection

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 //const path = require("path");
+const cors = require("cors");
 const connect = require("./connectMongo");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const User = require("./schema");
 
 app.post("/", async (req, res) => {
-    const notes = req.body;
-    console.log(notes);
+    const requestBody = req.body;
+    const newNotes = new User ({
+        author: requestBody.author,
+        notes: requestBody.notes,
+    });
+    await newNotes.save();
     return res.json(true);
 });
 
---
+++ front-end/create-note.js
@@ -0,0 +1,23 @@
+const { assertValidExecutionArguments } = require("graphql/execution/execute");
+
+const createNotes = () => {
+    const notesText = document.getElementById("note-text").value;
+    const authorText = document.getElementById("author-text").value;
+
+    const response = await axios({
+        url: "http://192.168.100.31:4300",
+        method: "POST",
+        data: {
+            notes: notesText,
+            author: authorText,
+        },
+    });
+
+    if(response.data === true) {
+        alert("Note successfully saved.")
+    } else {
+        alert ("Something wrong happened");
+    }
+
+
+}
